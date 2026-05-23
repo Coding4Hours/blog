8f6d653496200a6e7543a5ba8aa224e081ebb759
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-05-23 10:56:55 -0700

    feat: Initial Commit

---
+++ astro-paper.config.ts
@@ -0,0 +1,44 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://tu4l.one",
+    title: "Coding4hours",
+    description: "My personal blog.",
+    author: "Coding4hours",
+    profile: "https://tu4l.one",
+    ogImage: "default-og.jpg",
+    lang: "en",
+    timezone: "America/Los_Angeles",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: true,
+      url: "https://github.com/coding4hours/blog/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github", url: "https://github.com/coding4hours" },
+    { name: "discord", url: "https://discord.com/users/1208197331439063051" },
+    { name: "mail", url: "mailto:neealdon3@proton.me" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x", url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail", url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
